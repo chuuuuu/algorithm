715586ffee7a437c3b062d15cbf659435d9c2d5e
chuuuuu <33323263+chuuuuu@users.noreply.github.com>
2022-02-07 19:55:56 +0800

    update codejam

--- codejam/2021/1A/a/sol.cpp
+++ codejam/2021/1A/a/sol.cpp
@@ -1,110 +1,120 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using ll = long long;
-
-const ll N = 100;
-ll t, n;
-ll x[N];
-ll ans;
-
-ll head(ll a){
-  while(a > 9){
-    a /= 10;
-  }
-
-  return a;
-}
-
-ll digit(ll a){
-  ll ret = 1;
-  while(a>9){
-    a = a / 10;
-    ret += 1;
-  }
-
-  return ret;
-}
-
-void solve(){
-  cin >> n;
-  for(ll i=0; i<n; i++){
-    cin >> x[i];
-  }
-
-  ans = 0;
-  ll x_prev = 0;
-  for(ll i=0; i<n; i++){
-    if(x[i] > x_prev){
+string addOne(string str)
+{
+  reverse(str.begin(), str.end());
+  for (int i = 0; i < int(str.size()); i++)
+  {
+    int num = (str[i] - '0') + 1;
+    if (num < 10)
+    {
+      str[i] = '0' + num;
+      break;
     }
-    else{
-      ll d_prev = digit(x_prev);
-      ll d = digit(x[i]);
-      if(d_prev == d){
-        x[i] *= 10;
-        ans += 1;
-      }
-      else{
-        ll x_new = x[i];
-        for(ll i=0; i<d_prev-d; i++){
-          x_new *= 10;
-        }
-        ll diff = x_prev - x_new;
-        // cout << x_prev << ' ' << x_new << ' ' << digit(diff) << ' ' << d_prev << ' ' << d<< endl;
-        if(diff < 0){
-          x[i] = x_new;
-          ans += d_prev - d;
-        }
-        else if(digit(diff) <= (d_prev-d)){
-          ll x_tmp = x_prev + 1;
-          for(int j=0; j<d_prev-d; j++){
-            x_tmp /= 10;
-          }
-          if(x_tmp != x[i]){
-            x_new *= 10;
-            x[i] = x_new;
-            ans += d_prev - d + 1;
-          }
-          else{
-            x_new = x_prev + 1;
-            x[i] = x_new;
-            ans += d_prev - d;
-          }
-        }
-        else{
-          x_new *= 10;
-          x[i] = x_new;
-          ans += d_prev - d + 1;
-        }
-      }
-      // x[i] *= 10;
-      // ans += 1;
-      // while(x_prev - x[i] >= 9){
-      //   x[i] *= 10;
-      //   ans += 1;
-      // }
-      // if(x_prev >= x[i]){
-      //   x[i] = x_prev + 1;
-      // }
-      // x_prev = x[i];
+    if (num == 10)
+    {
+      str[i] = '0';
     }
-    x_prev = x[i];
   }
 
-  // for(ll i=0; i<n; i++){
-  //   cout << x[i] << ' ';
-  // }
-  // cout << endl;
+  if (str.back() == '0')
+  {
+    str.push_back('1');
+  }
+
+  reverse(str.begin(), str.end());
+  return str;
 }
 
-int main(){
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+string padding(string str, int size)
+{
+  for (int i = str.size(); i < size; i++)
+  {
+    str.push_back('0');
+  }
 
+  return str;
+}
+
+bool isGreater(string str1, string str2)
+{
+  if (str1.size() > str2.size())
+  {
+    return true;
+  }
+
+  if (str1.size() < str2.size())
+  {
+    return false;
+  }
+
+  for (int i = 0; i < int(str1.size()); i++)
+  {
+    if (str1[i] == str2[i])
+    {
+      continue;
+    }
+
+    return str1[i] > str2[i];
+  }
+
+  // equal
+  return false;
+}
+
+void solve()
+{
+  int N;
+  cin >> N;
+
+  vector<string> strs(N, "");
+  for (int i = 0; i < N; i++)
+  {
+    cin >> strs[i];
+  }
+
+  string prev = strs[0];
+  int ans = 0;
+  for (int i = 1; i < N; i++)
+  {
+    if (isGreater(strs[i], prev))
+    {
+      prev = strs[i];
+      continue;
+    }
+
+    string prevAddOne = addOne(prev);
+    if (prevAddOne.substr(0, strs[i].size()) == strs[i])
+    {
+      prev = prevAddOne;
+      ans += prevAddOne.size() - strs[i].size();
+      continue;
+    }
+
+    string strPadding = padding(strs[i], prev.size());
+    if (!isGreater(strPadding, prev))
+    {
+      strPadding.push_back('0');
+    }
+
+    prev = strPadding;
+    ans += strPadding.size() - strs[i].size();
+  }
+
+  cout << ans << "\n";
+}
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  auto t = 0;
   cin >> t;
-  for(ll i=0; i<t; i++){
-    cout << "Case #" << i+1 << ": ";
+  for (int i = 0; i < t; i++)
+  {
+    cout << "Case #" << i + 1 << ": ";
     solve();
-    cout << ans << endl;
   }
 }
