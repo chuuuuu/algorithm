12981f88ee094ae6fb808752990fa0fac685990f
chuuuuu <33323263+chuuuuu@users.noreply.github.com>
2022-03-03 18:12:41 +0800

    create codejam template

---
+++ codejam/template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+  int ans = 0;
+  cout << ans << "\n";
+}
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int t = 0;
+  cin >> t;
+  for (int i = 0; i < t; i++)
+  {
+    cout << "Case #" << i + 1 << ": ";
+    solve();
+  }
+}
